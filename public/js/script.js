5a15eb3159509f1dacaff7bf2c50d467e75bdb09
Marcus Edidiong Cletus <edi_mars@outlook.com>
2025-12-26 05:56:09 +0100

    modal

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,17 @@ const dropMenuEl = document.getElementById("dropMenu");
 const asideEl = document.getElementById("aside");
 const closeEl = document.getElementById("close");
 const dropBtnEl = document.querySelector(".dropBtn");
-const contentsEl = document.querySelector(".contents");
+const signupLoginEl = document.querySelector("#signupLogin");
+const authCloseEl = document.getElementById("auth-close");
+
+// ABOUT MODAL
+const aboutModalEl = document.getElementById("modalAbout");
+const aboutContainerEl = document.getElementById("about-container");
+
+// STRENGTHS
+const openModalBtnEl = document.getElementById("openModalBtn");
+const modalOverlay = document.getElementById("modalOverlay")
+const closeModalBtnEl = document.querySelector(".close-btn");
 
 
 
@@ -20,22 +30,72 @@ function closeMenu() {
 }
 
 //Login Btn on Navbar
-dropBtnEl.addEventListener("click", () => {
-  contentsEl.style.display = "block";
+function openDropBtn() {
+  signupLoginEl.style.display = "block";
+  
+  setTimeout(() => {
+    signupLoginEl.classList.add('show');
+  }, 10);
+}
+
+function authClose() {
+  signupLoginEl.classList.remove("in-active");
+  
+  setTimeout(() => {
+    signupLoginEl.style.display = "none";
+  }, 10);
+}
+
+window.addEventListener("click", (e) => {
+  if(e.target === signupLoginEl) {
+    authClose();
+  }
 });
+
+//Button on click
+dropBtnEl.addEventListener("click", openDropBtn);
+authCloseEl.addEventListener("click", authClose);
 
 function contentsClose() {
 
 }
 
 
-// About Page
-const aboutModalEl = document.getElementById("modalAbout");
-const aboutContainerEl = document.getElementById("about-container");
 
+//ABOUT MODAL
 aboutModalEl.addEventListener("click", (e) => {
   e.preventDefault();
   aboutContainerEl.classList.add("show"); 
+});
+
+//STRENGTH MODAL
+function openModal() {
+  // Make it visible immediately (display: flex) so transition can start
+  modalOverlay.style.display = 'flex';
+  // Use a slight delay to ensure browser reisters the 'display' change
+  //before adding the 'active' class to trigger the transition
+  setTimeout(() => {
+    modalOverlay.classList.add('active');
+  }, 10);
+}
+
+function closeModal() {
+  modalOverlay.classList.remove('active');
+  // wait for the transition to finish before hidding it (display: none;)
+  setTimeout(() => {
+    modalOverlay.style.display = 'none';
+  }, 300); // Must match th CSS transition duration (0.3s or 300ms)
+}
+
+// Event Listeners
+openModalBtnEl.addEventListener('click', openModal);
+closeModalBtnEl.addEventListener('click', closeModal);
+
+// Close the modal if the user clicks outside the modal content (on the overlay)
+modalOverlay.addEventListener('click', (e) => {
+  if(e.target === modalOverlay) {
+    closeModal();
+  }
 });
 
 
